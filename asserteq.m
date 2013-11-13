b1e4f87d09ccb13507d6df57fc2dd688ce93d5d8
unknown <andrew.sparkes@BH145_4197.oit.edu>
2013-11-13 14:10:03 -0800

    fixed asserteq

---
+++ asserteq.m
@@ -0,0 +1,12 @@
+function [ ] = asserteq(a, b, msg)
+%     pass = (a == b);
+%     if isscalar(pass)
+%         % do nothing
+%     elseif isvector(pass)
+%         pass = prod(pass);
+%     else
+%         error('a and b must be scalars or vectors only')
+%     end
+%     assert(pass, msg)
+    assert(prod(a==b)==1, msg)
+end
